1e1791f6d2efd194ceaf5451987ba7f7b636fc2e
Gabriell1507 <gabrielmarcosbguimaraes@gmail.com>
2023-11-21 15:11:51 -0300

    feat: função de expandir a imagem em desenvolvimento

--- js/main.js
+++ js/main.js
@@ -4,13 +4,29 @@ const btnCloseModal2 = document.getElementById("btn-close-modal-2");
 const modal = document.getElementById("modalSheet");
 
 btnEbook.addEventListener("click", () => {
-    modal.classList.remove("ocultar-modal");
+  modal.classList.remove("ocultar-modal");
 });
 
 btnCloseModal.addEventListener("click", () => {
-    modal.classList.add("ocultar-modal");
+  modal.classList.add("ocultar-modal");
 });
 
 btnCloseModal2.addEventListener("click", () => {
-    modal.classList.add("ocultar-modal");
+  modal.classList.add("ocultar-modal");
 });
+
+function expandirImagem(elemento) {
+  const divExpandida = document.createElement("div");
+  divExpandida.className = "expanded";
+
+  const imgExpandida = document.createElement("img");
+  imgExpandida.src = elemento.src;
+
+  divExpandida.appendChild(imgExpandida);
+
+  document.body.appendChild(divExpandida);
+
+  divExpandida.addEventListener("click", function () {
+    document.body.removeChild(divExpandida);
+  });
+}
